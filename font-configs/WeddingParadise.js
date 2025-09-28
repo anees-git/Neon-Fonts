5d2be6b9c9faef4e55d3b5e0ddef2f0a943098d1
anees-git <fotoindustry.pk@gmail.com>
2025-09-29 02:52:22 +0500

    Create WeddingParadise.js

---
+++ font-configs/WeddingParadise.js
@@ -0,0 +1,5 @@
+export const WeddingParadise = {
+  name: "WeddingParadise",
+  fontFamily: "WeddingParadise",
+  url: "https://github.com/anees-git/Neon-Fonts/raw/main/fonts/WeddingParadise.ttf"
+};
